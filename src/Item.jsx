79f72df2367104d9f656227f4f6bd4d4ecc9169d
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-03-21 21:23:46 +0800

    feat: add product title, price and description

--- src/Item.jsx
+++ src/Item.jsx
@@ -7,13 +7,13 @@ import outfit4 from './assets/outfit4.png';
 export default function Item(){
     
     return (
-        <div className="flex flex-col item-center text-h1 font-bold pt-[34px] px-[21px] gap-5">
-            <div className="w-[330.73px] h-[371.07px] flex flex-col justify-between items-center">
+        <div className="flex flex-col items-center pt-[34px] px-[21px] gap-5">
+            <div className="w-[330.73px] h-full flex flex-col justify-between items-center">
                 <div className="w-full h-auto flex justify-center p-[5.04px] gap-[5.04px]">
                     <img 
                         src={outfit1} 
                         alt="main image"
-                        className="w-[320.65px] h-[360.98] rounded-[8.07px] object-cover"
+                        className="w-[320.65px] h-[360.98px] rounded-[8.07px] object-cover"
                     />
                 </div>
                 <div className="w-full flex justify-center gap-[10.59px]">
@@ -37,8 +37,22 @@ export default function Item(){
                         />
                 </div>
             </div>
-            <div className="flex flex-col gap-5">
-
+            <div className="w-full flex flex-col gap-5">
+                <h4 className="text-h4 font-[600] text-black">
+                    Long Sleeve Overshirt Hoodie
+                </h4>
+                <div className="flex gap-2.5 items-center">
+                    <span className="text-h5 font-[600] text-gray line-through">$535.00</span>
+                    <span className="text-h4 font-[600] text-black">$489.00</span>
+                </div>
+                <div className="flex flex-col gap-[15px]">
+                    <h6 className="text-h6 font-bold text-black">Description</h6>
+                    <p className="text-p text-gray">Effortlessly stylish and versatile, this Long Sleeve Overshirt in perfect for layering or wearing solo. Crafted for comfort and durability, it offers a timeless look that complements any wardrobe. Ideal for casual outings or smart-casual occasions, this overshirt is a must-have staple.
+                    </p>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
             <div>
 
